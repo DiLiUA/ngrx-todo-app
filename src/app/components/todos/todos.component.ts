420360075a1a73c0f0677e1029002233642dd4c5
d.litvinov <dmutro.litvinov@gmail.com>
2017-06-28 16:36:03 +0300

    feat(todo): add todo and toggle todo is ready for test

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { TodosActions } from '../../actions/todos.actions';
+import { TodosState } from '../../reducers/todos.reducer';
+import { Todo } from '../../models/todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html'
+})
+export class TodosComponent {
+  textTodo: string;
+  todoItems: Observable<Todo[]>;
+
+  constructor(private store: Store<TodosState>, private actions: TodosActions) {
+    const todos: Observable<TodosState> = this.store.select('todos');
+
+    this.textTodo = '';
+    this.todoItems = todos.map(tds => tds.todoItems);
+    this.store.dispatch(this.actions.getTodos());
+  }
+
+  submitTodo(evt: Event): void {
+    event.preventDefault();
+    if (this.textTodo) {
+      this.store.dispatch(this.actions.addTodo(this.textTodo));
+      this.textTodo = '';
+    }
+  }
+}
---
+++ src/app/services/todos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+import { Todo } from '../models/todo';
+
+@Injectable()
+export class TodosService {
+  todos: Todo[];
+
+  constructor() {
+    this.todos = [{text: 'todo 1', isDone: false}, {text: 'todo 2', isDone: true}, {text: 'todo 3', isDone: false}];
+  }
+
+  getTodos(): Observable<Todo[]> {
+    return Observable.of(this.todos);
+  }
+
+  addTodo(text: string): Observable<Todo> {
+    return Observable.of({text, isDone: false});
+  }
+
+  toggleDoneTodo(todo: Todo): Observable<Todo> {
+    return Observable.of(todo);
+  }
+}
