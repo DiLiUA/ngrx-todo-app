0d01d3a6077b962d907ac64a31f118377724d7d8
d.litvinov <dmutro.litvinov@gmail.com>
2017-07-18 18:05:15 +0300

    e2e tests is ready for test

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,46 @@
-import { browser, by, element } from 'protractor';
+import { $, $$, browser, ElementArrayFinder, ElementFinder } from 'protractor';
+import { promise } from 'selenium-webdriver';
+import Promise = promise.Promise;
 
 export class NgrxTodoAppPage {
-  navigateTo() {
+
+  navigateTo(): Promise<string>  {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getParagraphText(): Promise<string>  {
+    return $('app-todos h3').getText();
+  }
+
+  getTodos() {
+    return $$('app-todo > div');
+  }
+
+  getRemoveTodoBtn(todo: ElementFinder): ElementFinder {
+    return todo.$('.remove-icon');
+  }
+
+  getEditTodoBtn(todo: ElementFinder): ElementFinder {
+    return todo.$('.edit-icon');
+  }
+
+  getCheckboxTodo(todo: ElementFinder): ElementFinder {
+    return todo.$('input[type="checkbox"]');
+  }
+
+  getTextTodo(todo: ElementFinder): Promise<string> {
+    return todo.$('label').getText();
+  }
+
+  getFilters(): ElementArrayFinder {
+    return $$('.filters li');
+  }
+
+  getInputTodo() {
+    return $('app-todos input[name="todo"]');
+  }
+
+  getBtnAddTodo() {
+    return $('app-todos button[type="submit"]');
   }
 }
